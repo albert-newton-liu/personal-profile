bd5b5e6cd07f20954b2ac58523096e16293468d7
Albert <savice1209@163.com>
2025-09-13 22:52:42 +1200

    init project

---
+++ src/test/java/com/albert/liuton/personalprofile/PersonalProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.liuton.personalprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
